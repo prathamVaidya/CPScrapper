9e037e44af6446ad9a1ca02aa4bbdf8290d0afb5
Aman <amanuniquecoder@gmail.com>
2021-09-15 01:04:40 +0530

    Created Atcoder

--- webscrapers/example.js
+++ webscrapers/example.js
@@ -1,13 +1,26 @@
 var codechef = require("./codechef");
 var codeforces = require("./codeforces");
+var atcoder = require("./atcoder");
 
 /* Get Upcoming Contests of codechef */
 
-codechef.upcomingContests(function (data) {
-    console.log(data);
-})
+ codechef.upcomingContests(function (data) {
+     console.log(data);
+ }) 
 
 /* Get Upcoming Contests of codeforce */
-// codeforces.upComingContest(function (data) {
-//   console.log(data);
-// });
+codeforces.upComingContest(function (data) {
+  console.log(data);
+}); 
+
+atcoder.upComingContest(function (data) {
+  console.log(data);
+});
+
+atcoder.preComingContest(function (data) {
+  console.log(data);
+});
+
+atcoder.pastContest(function (data) {
+  console.log(data);
+}); 
